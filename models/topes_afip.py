7dcd746c6d5ead52a467172f8b602a0e9e7e626b
GuvensConsultora <guvensconsultora@gmail>
2025-07-10 21:54:46 -0300

    Corrigo el string del campo tope

--- models/topes_afip.py
+++ models/topes_afip.py
@@ -5,7 +5,7 @@ class TopeConsFinal(models.Model):
     _inherit = 'res.company'
     _description = 'Agregar agregar el campo para establecer el tope de consumidor final'
 
-    x_tope_cf = fields.Float(String="Tope Consumidor Final",help=(
+    x_tope_cf = fields.Float(string="Tope Consumidor Final",help=(
             "Importe máximo que puede facturarse a un Consumidor Final anónimo. "
             "Si la operación supera este monto, el sistema solicitará identificar "
             "al comprador (CUIT/CUIL/DNI) conforme RG 5700/2025."
