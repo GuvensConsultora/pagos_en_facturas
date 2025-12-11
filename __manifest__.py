cec39cfe3a95759ef8f7f9911f68c387bce5951a
guvensconsultora <guvensconsultora@gmail.com>
2025-12-11 10:34:28 -0300

    Actualizar pagos en facturas v 5.1

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': "pagos_en_facturas",
     'version': '17.0',
     'category': 'Custom',
-    'depends': ['base','account'],
+    'depends': ['base','account','delivery'],
     'data': ['views/view.xml',
              'security/ir.model.access.csv',
              'views/res_partner_view.xml',
